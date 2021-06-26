ac73597966afbe5e78e77782e381b8e3fc1b3071
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-26 16:27:10 +0430

    Rendering template

---
+++ app/app.py
@@ -0,0 +1,15 @@
+# Flask Library
+from flask import Flask, render_template
+
+# App Configuration
+app = Flask(__name__)
+
+# Route Index
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# Running App
+if __name__ == "__main__":
+    app.run("0.0.0.0", 2000)
